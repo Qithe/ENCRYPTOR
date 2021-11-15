6d93e94ab8ee23b5876feffe8b825213bfa13c38
Oscar Wolter <OscarDaWolter@gmail.com>
2021-11-15 17:20:48 +0100

    Add project files.

---
+++ Pirate_encryptor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ENCRYPTOR
+{
+    class Pirate_encryptor
+    {
+        private char filler = (char)111;
+        public  string Encrypt(string input)
+        {
+            Char[] arr = MakeArray(input);
+            Char[] newArr;
+            List<char> workList = new List<char>();
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                workList.Add((char)arr.GetValue(i));
+                if (!IsVowel((char)arr.GetValue(i)))
+                {
+                    workList.Add(filler);
+                    workList.Add((char)arr.GetValue(i));
+
+                }
+            }
+
+            newArr = workList.ToArray();
+            return (new string(newArr));
+        }
+
+        public  string Decrypt(string input)
+        {
+            Char[] arr = MakeArray(input);
+            Char[] newArr;
+            List<char> workList = new List<char>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!IsVowel((char)arr.GetValue(i)))
+                {
+                    if (i+1!=arr.Length)
+                    {
+                        if ((char)arr.GetValue(i + 1) == filler && (char)arr.GetValue(i + 2) == (char)arr.GetValue(i))
+                        {
+                            workList.Add((char)arr.GetValue(i));
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    if ((char)arr.GetValue(i) == filler)
+                    {
+                        if ((char)arr.GetValue(i + 1) != (char)arr.GetValue(i - 1))
+                        {
+                            workList.Add((char)arr.GetValue(i));
+                        }
+                    }
+                    else
+                    {
+                        workList.Add((char)arr.GetValue(i));
+                    }
+                }
+            }
+
+            newArr = workList.ToArray();
+            return (new string(newArr));
+        }
+
+        private Char[] MakeArray(string input)
+        {
+            return (input.ToCharArray());
+        }
+
+        private bool IsVowel(char c)
+        {
+            return "aeiouAEIOU".IndexOf(c) >= 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ENCRYPTOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pirate_encryptor pirate = new Pirate_encryptor();
+            string x = pirate.Encrypt("Hello World");
+            Console.WriteLine(x);
+            Console.WriteLine(pirate.Decrypt(x));
+        }
+
+
+    }
+}
