c5b6a690b421c6711506fb8978b6abcf5616163e
Oscar Wolter <OscarDaWolter@gmail.com>
2021-11-15 17:34:19 +0100

    Begun work on being able to read txt files

---
+++ FileHandeler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENCRYPTOR
+{
+    class FileHandeler
+    {
+        public string Reader(string filepath)
+        {
+            return (System.IO.File.ReadAllText(filepath));
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace ENCRYPTOR
         static void Main(string[] args)
         {
             Pirate_encryptor pirate = new Pirate_encryptor();
+            FileHandeler reader = new FileHandeler();
             string x = pirate.Encrypt("Hello World");
             Console.WriteLine(x);
             Console.WriteLine(pirate.Decrypt(x));
+            Console.WriteLine(reader.Reader(@"C:\Users\Oscar\Desktop\Test.txt"));
+
         }
 
 
